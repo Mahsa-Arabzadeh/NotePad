c09489645b09255adcfb7a1263c8d1434cfb5909
mahsa <arabzadehmah@gmail.com>
2023-08-15 18:22:31 +0430

    js complete (we can add note)

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-const btnAdd = document.querySelector(".add-file");
-const newDiv = document.querySelector("#check-box");
-function getNote() {
-  if (
-    btnAdd.addEventListener("click", function () {
-      let newDiv = document.createElement("div");
-      document.querySelector("#check-box").appendChild(newDiv);
-      newDiv.textContent = "Add a note";
-    })
-  ) {
-  }
-}
-getNote();
+const inputTitle = document.querySelector("#title-note");
+const addBtn = document.querySelector(".add-task");
+const inputText = document.querySelector("#NotePad");
+const output = document.querySelector("#check-box");
 
-console.log("mahsa");
+let child, delBtn, h2, p;
+
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if ((inputTitle.value == "") | (inputText.value == " ")) {
+    alert("enter title");
+  } else if ((inputText.value == "") | (inputTitle.value == " ")) {
+    alert("enter note");
+  } else {
+    child = document.createElement("div");
+    delBtn = document.createElement("button");
+    h2 = document.createElement("h2");
+    p = document.createElement("p");
+
+    h2.innerHTML = inputTitle.value;
+    p.innerHTML = inputText.value;
+    delBtn.innerHTML = "Delete";
+
+    child.classList.add("lists");
+    delBtn.classList.add("delete");
+    h2.classList.add("output-title");
+    p.classList.add("text");
+
+    child.appendChild(delBtn);
+    child.appendChild(h2);
+    child.appendChild(p);
+    output.appendChild(child);
+
+    inputTitle.innerHTML = "";
+    inputText.innerHTML = "";
+  }
+
+  delBtn.addEventListener("click", function () {
+    output.removeChild(this.parentElement);
+  });
+});
